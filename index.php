d91cfe5123ca981c2d122e84c1ed6b4d36a92511
moises <moisessouza0204@gmail.com>
2022-09-28 02:19:19 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="script\taiwild.js"></script>
+    <script src="script\tailwind.config.js"></script>
+    
+    <title>Simulado</title>
+</head>
+<body>
+    <div class="flex h-screen w-screen bg-[#cce2e2] items-center justify-center">
+        <div class="w-1/3">
+            <div class="flex justify-center">
+                <img src="img/techman.png" alt="logo"> 
+            </div>
+            
+            <div class="p-8 bg-[#eeedeb] ">
+                <div class="flex justify-center">
+                    <input style="-webkit-text-security: square;" type="password" class="flex items-center border-b border-teal-500 py-2 w-screen text-[#35797d]">
+                </div>
+                <div class="grid grid-cols-3 grid-rows-4 gap-2 mt-8 justify-items-center px-26">
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">1</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">2</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">3</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">4</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">5</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">6</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">7</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">8</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">9</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">C</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">0</div>
+                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center">ENTER</div>
+                </div>  
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ script/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'azul-22': '#cce2e2',
+        },
+      }
+    }
+  }
