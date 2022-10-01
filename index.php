94642ab2412af88dc23284b1ee6dffdd891d6758
moises <moisessouza0204@gmail.com>
2022-10-01 01:27:16 -0300

    banco and scripts

--- index.php
+++ index.php
@@ -1,42 +1,70 @@
 <!DOCTYPE html>
-<html lang="pt-br>
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
+    <meta charset="UTF-8" http-equiv="X-UA-Compatible" content="IE=edge">
+ 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="script\taiwild.js"></script>
     <script src="script\tailwind.config.js"></script>
-    
+    <link rel="stylesheet" href="css/style.css">
+   
     <title>Simulado</title>
 </head>
 <body>
     <div class="flex h-screen w-screen bg-[#cce2e2] items-center justify-center">
         <div class="w-1/3">
             <div class="flex justify-center">
-                <img src="img/techman.png" alt="logo"> 
+                <img src="img/techman.png" alt="logo">
             </div>
-            
+           
             <div class="p-8 bg-[#eeedeb] ">
                 <div class="flex justify-center">
-                    <input style="-webkit-text-security: square;" type="password" class="flex items-center border-b border-teal-500 py-2 w-screen text-[#35797d]">
+                    <input id="resultado" method="POST" class="flex items-center border-b border-teal-500 py-2 w-screen text-[#35797d]" style="outline: none;">
                 </div>
                 <div class="grid grid-cols-3 grid-rows-4 gap-2 mt-8 justify-items-center px-26">
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">1</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">2</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">3</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">4</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">5</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">6</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">7</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">8</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">9</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">C</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">0</div>
-                    <div class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">ENTER</div>
+                            <button onclick="insert('1')" class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">1</button>
+                            <button onclick="insert('2')"  class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">2</button>
+                            <button onclick="insert('3')"  class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">3</button>
+                            <button onclick="insert('4')"  class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">4</button>
+                            <button onclick="insert('5')"  class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">5</button>
+                            <button onclick="insert('6')"  class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">6</button>
+                            <button onclick="insert('7')"  class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">7</button>
+                            <button onclick="insert('8')"  class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">8</button>
+                            <button onclick="insert('9')"  class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">9</button>
+                            <button onclick="clean()" class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">C</button>
+                            <button onclick="insert('0')"  class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">0</button>
+                            <button id="login" onclick="logar()" class="rounded-full bg-white border-[#35797d] text-[#35797d] hover:bg-[#35797d] hover:text-white border-2 w-16 h-16 flex items-center justify-center" style="cursor:pointer;">ENTER</button>
                 </div>  
             </div>
         </div>
     </div>
+ 
+    <script>
+       
+    </script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script>
+    variavel = ""
+    function insert(num){
+        variavel = document.getElementById('resultado').value + num
+        console.log(variavel)
+        document.getElementById('resultado').value = variavel
+    }
+
+    function clean(){
+        document.getElementById('resultado').value = ""
+    }
+
+    function logar(){
+        var login = document.getElementById('login').value = ""
+        variavel = document.getElementById('resultado').value
+        document.getElementById('resultado').value = variavel
+        if(variavel == "777"){
+            location.href = "novoEquipamento.php";
+        }else{
+            alert("Senha incorreta")
+        }
+    }
+    </script>
 </body>
 </html>
--- novoEquipamento.php
+++ novoEquipamento.php
@@ -1,18 +1,18 @@
 <?php
-
+ 
 $bd = new SQLite3("db/equipamentos.db");
 $sql = "SELECT * FROM equipamentos";
 $equipamentos = $bd->query($sql);
-
+ 
 $bd1 = new SQLite3("db/comentarios.db");
 $sql1 = "SELECT * FROM comentarios";
 $comentarios = $bd1->query($sql1);
 ?>
-
-
+ 
+ 
 <!DOCTYPE html>
 <html lang="pt-br">
-
+ 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,16 +22,16 @@ $comentarios = $bd1->query($sql1);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link rel="import" href="telaModal.php">
     <link rel="stylesheet" href="../css/style.css">
-
+ 
     <title>Document</title>
 </head>
-
+ 
 <body class="bg-[#F6F5F9]">
     <div class="p-2 h-20 bg-[#CCE2E2] flex justify-between">
         <img src="../img/techman.png" alt="logo">
         <div class="flex m-2 flex items-center">
             <b class="text-xl m-8 text-[#35797d]" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor:pointer;">Novo equipamento</b>
-            <img class="flex w-8 h-8" src="../img/logout_sair.png" style="cursor:pointer;" alt="sair">
+            <a href="index.php"><img class="flex w-8 h-8" l src="../img/logout_sair.png" style="cursor:pointer;" alt="sair"></a>
         </div>
     </div>
     <!-- Equipamentos -->
@@ -47,12 +47,12 @@ $comentarios = $bd1->query($sql1);
             <img class="icon" type="button" data-bs-toggle="modal" data-bs-target="#exampleModa3"  
             data-id-apagar="<?php echo $equipamento["id"]?>" src="../img/deletar.png" alt="deletar"
             >
-
+ 
             <hr style="margin-top: 100px;">
         </div>
         <?php endwhile ?>
     </section>
-
+ 
 <div>
     <!-- MODEL NOVO EQUIPAMENTO -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -85,7 +85,7 @@ $comentarios = $bd1->query($sql1);
             </div>
         </div>
     </div>
-
+ 
     <!-- MODEL COMENTARIO -->
     <div class ="coment">
         <div class="modal fade" id="exampleModa2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -113,8 +113,8 @@ $comentarios = $bd1->query($sql1);
             </div>
         </div>
     </class>
-
-
+ 
+ 
     <!-- MODEL ADICIONAR COMENTARIO -->
     <div class="modal fade" id="exampleModa4" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg black">
@@ -138,8 +138,8 @@ $comentarios = $bd1->query($sql1);
             </div>
         </div>
     </div>
-
-
+ 
+ 
     <!-- MODEL DELETAR -->
         <div class="modal fade" id="exampleModa3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-lg black">
@@ -162,10 +162,10 @@ $comentarios = $bd1->query($sql1);
                 </div>
             </div>
         </div>
-        
+       
     </body>
 <!-- Script apagar equipamento -->
-
+ 
 <!-- <script>
     document.querySelectorAll(".btn-excluir").forEach(btn=>{
         btn.addEventListener("click", e=>{
@@ -177,18 +177,18 @@ $comentarios = $bd1->query($sql1);
                     if (response.success ==="ok") {
                         console.log("apagado com sucesso")
                     }
-                    
+                   
                 })
         });
     });
-    
-</script>  --> 
-
-
+   
+</script>  -->
+ 
+ 
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script>
        
-
+ 
     const exampleModal = document.getElementById('exampleModa3')
     exampleModal.addEventListener('show.bs.modal', event => {
       const button = event.relatedTarget
@@ -197,7 +197,7 @@ $comentarios = $bd1->query($sql1);
     const modalBodyInput = exampleModal.querySelector('.modal-body input')
   modalTitle.textContent = `New message to ${recipient}`
   modalBodyInput.value = recipient */
-
+ 
   $("#confirmar-apagar").submit(function(e) {
         e.preventDefault();
         $.ajax({
@@ -213,8 +213,9 @@ $comentarios = $bd1->query($sql1);
           })})
 })
 </script>
-
-
+ 
+ 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
-
-</html> 
+ 
+</html>
+ 
